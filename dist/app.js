f61eebec1c160d2d3b705dcf85438eca95536c70
Thomas Lebel <thomas.lebel38@gmail.com>
2025-03-05 18:37:20 +0100

    compilation tsc

--- dist/app.js
+++ dist/app.js
@@ -9,13 +9,16 @@ const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const connection_1 = require("./models/connection");
 const index_1 = __importDefault(require("./routes/index"));
 const users_1 = __importDefault(require("./routes/users"));
 const products_1 = __importDefault(require("./routes/products"));
 const orders_1 = __importDefault(require("./routes/orders"));
 const app = (0, express_1.default)();
+// Connexion à la base de données
+(0, connection_1.connectDatabase)().catch(console.error);
 // Configuration de fileUpload
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 // Configuration de CORS
 app.use((0, cors_1.default)());
@@ -29,5 +32,5 @@ app.use((0, cookie_parser_1.default)());
 app.use("/", index_1.default);
 app.use("/users", users_1.default);
 app.use("/products", products_1.default);
-app.use('/orders', orders_1.default);
+app.use("/orders", orders_1.default);
 exports.default = app;
