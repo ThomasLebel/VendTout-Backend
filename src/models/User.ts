3a5b12662cb1ec9e60e5a3301470b76fb942ac87
Thomas Lebel <thomas.lebel38@gmail.com>
2025-03-13 12:23:56 +0100

    Changement lien image profil par défaut

--- src/models/User.ts
+++ src/models/User.ts
@@ -26,7 +26,7 @@ const userSchema = new Schema<IUser>(
     token: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     aboutDescription: { type: String, required: false, default: "" },
-    profilePicture: { type: String, required: false, default: "https://res.cloudinary.com/dkf48p2ah/image/upload/v1739526042/idkhe6v85woa3fdoszls.jpg" },
+    profilePicture: { type: String, required: false, default: "https://res.cloudinary.com/dkf48p2ah/image/upload/v1739809289/VendToutAvatars/mk8ihczepktfn61qdzh1.jpg" },
     country: { type: String, required: false, default: "" },
     city: { type: String, required: false, default: "" },
     gender: { type: String, required: false, default: "" },
